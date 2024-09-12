c42e40127b512a09233038341d49367cea0d9d9d
metametaphysician <preston@logoi.website>
2024-09-12 21:54:50 +0000

    miscellaneous desiderata

--- editor/editor.js
+++ editor/editor.js
@@ -1,1 +1,8 @@
-alert(`editor.js loaded successfully`);
+let editor = document.getElementById("editor");
+
+for (pane in ["left", "right"]) {
+    let pane_div = document.createElement("div");
+    pane_div.setAttribute("id", pane);
+    pane_div.classList.add("half");
+    editor.appendChild(pane_div);
+}
