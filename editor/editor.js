45369c73d94208e820aecfd03095a13a15de9219
metametaphysician <preston@logoi.website>
2024-09-12 22:55:23 +0000

    miscellaneous desiderata

--- editor/editor.js
+++ editor/editor.js
@@ -23,6 +23,7 @@ for (let i = 0; i < editor_rows; i++) {
     for (let j = 0; j < editor_columns; j++) {
         let column = document.createElement("td");
         if (j == 0) {
+            let line_number = (i < 10) ? String() : String();
             column.appendChild(document.createTextNode(String(i)));
             column.classList.add("gutter");
         }
